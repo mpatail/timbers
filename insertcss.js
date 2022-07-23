877ec9ce4151ea15af6ab8a3b95f0b61caf38e10
mpatail <109394932+mpatail@users.noreply.github.com>
2022-07-23 11:01:41 -0700

    Update insertcss.js

--- insertcss.js
+++ insertcss.js
@@ -1,7 +1,2 @@
-    window.addEventListener("load", () => {
-      // (B) GET HTML ELEMENT
-      var newcsss = document.getElementById(“body”);
-      console.log(newcss);
-
-      // (D) THIS WILL APPEND TO THE CONTENTS
-      newcss.innerHTML += " is <strong>NOT</strong> John Wick.";
+var newcsss = document.getElementById(“body”).innerHTML = 'CHARA WAS HERE';
+console.log(newcss);
